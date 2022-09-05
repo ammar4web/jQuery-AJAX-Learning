5357e0966a7ff139a0baaece77291083387df9a2
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-05 20:09:47 +0300

    jQuery-Effects: .show(500) | .hide() | .toggle() | .slideToggle()

---
+++ jQueryJavaScrip/effects.js
@@ -0,0 +1,12 @@
+$("#show").click(function () {
+  $("#content").show(500);
+});
+$("#hide").click(function () {
+  $("#content").hide(1000);
+});
+$("#toggle").click(function () {
+  $("#content").toggle(3000);
+});
+$("#slideToggle").click(function () {
+  $("#content").slideToggle(3000);
+});
