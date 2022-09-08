17d010c976022487ab437c8fe76a79e5c21361d6
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-08 12:53:41 +0300

    AJAX: XHR XML Http Request.  JSON.parse()

--- AJAX_JavaScript/xhr.js
+++ AJAX_JavaScript/xhr.js
@@ -5,7 +5,20 @@ let myRequest = new XMLHttpRequest();
 myRequest.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     // responseText
-    console.log(this.responseText);
+    // console.log(this.responseText);
+    let myJsObject = JSON.parse(this.responseText),
+      MyText = "";
+
+    for (let Object of myJsObject) {
+      // console.log(Object.id, Object.userId);
+      MyText += "<tr>";
+      MyText += "<td>" + Object.id + "</td>";
+      MyText += "<td>" + Object.userId + "</td>";
+      MyText += "<td>" + Object.title + "</td>";
+      MyText += "<td>" + Object.completed + "</td>";
+      MyText += "</tr>";
+    }
+    $("tbody").append(MyText);
   }
 };
 
